4679849e836de15b6c5327afa082b95ede798514
calvin <calvinchun@naver.com>
2022-08-11 17:28:31 +0900

    commiton0811chun

---
+++ src/main/java/com/assistant/trial/ChunController.java
@@ -0,0 +1,21 @@
+package com.assistant.trial;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ChunController {
+	
+	@GetMapping("/chun1")
+	public String chun1() {
+		
+		return "helper_input/helper_input_indiv";
+	}
+	
+	@GetMapping("/chun2")
+	public String chun2() {
+		
+		return "helper_input/helper_input_com_lookup";
+	}
+	
+}
