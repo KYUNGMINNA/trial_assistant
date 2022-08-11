511e2b76ba73d397a35b41f76b4f2713dbe15d5b
calvin <calvinchun@naver.com>
2022-08-11 14:32:21 +0900

    0811classnamechange

--- src/main/java/com/assistant/trial/ChunController.java
+++ src/main/java/com/assistant/trial/ChunController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class chun {
+public class ChunController {
 
 	@GetMapping("/chun1")
 	public String chun1() {
