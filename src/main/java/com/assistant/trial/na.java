10a9a1692738a060a3f08751b6e35ade7a7883ce
KYUNGMIN <62867182+KYUNGMINNA@users.noreply.github.com>
2022-08-12 14:46:41 +0900

    Update na.java

--- src/main/java/com/assistant/trial/na.java
+++ src/main/java/com/assistant/trial/na.java
@@ -6,13 +6,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class na {
 
+	//조력자 공고 상세 내용
 	@GetMapping("/na1")
 	public String na1() {
 		
 		return "article/article_content";
 	}
 	
-
+	//조력자 공고 목록
 	@GetMapping("/na2")
 	public String na2() {
 		
@@ -20,46 +21,26 @@ public class na {
 	}
 	
 
+	//관리자가 조력자 공고 모집 게시글 작성 화면
 	@GetMapping("/na3")
 	public String na3() {
 		
 		return "article/article_regist";
 	}
-
+	
+	//사용자의 조력자 신청 화면
 	@GetMapping("/na4")
 	public String na4() {
 		
 		return "helper_apply/helper_apply";
 	}
 
+	//관리자가 조력자 신청을 접수한 사람들 목록 조회
 	@GetMapping("/na5")
 	public String na5() {
 		
 		return "admin_inquiry/admin_helper_inquiry";
 	}
 
-	@GetMapping("/na6")
-	public String na6() {
-		
-		return "";
-	}
-
-	@GetMapping("/na7")
-	public String na7() {
-		
-		return "";
-	}
-
-	@GetMapping("/na8")
-	public String na8() {
-		
-		return "";
-	}
-
-	@GetMapping("/na9")
-	public String na9() {
-		
-		return "";
-	}
 	
 }
